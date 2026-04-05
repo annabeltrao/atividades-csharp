8b613a2b31f037e8cbd150b4e8be082c67afc326
Anna Beltrão <abeltraomoreira@gmail.com>
2026-04-05 18:28:59 -0300

    Add files via upload

---
+++ atividades/exercicios-arquivo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class arquivo
+{
+    static void Main()
+    {
+        string caminho = "text.txt";
+
+        if (File.Exists(caminho))
+        {
+            Console.WriteLine("O arquivo existe!");
+
+
+            //Lê o conteudo do arquivo 
+            string conteudo = File.ReadAllText(caminho);
+            Console.WriteLine("O conteudo do arquivo:");
+            Console.WriteLine(conteudo);
+        }
+
+        //  Se o arquivo não existir
+        else
+        {
+            Console.WriteLine("O arquivo não existe.");
+        }
+
+        // Pedir para o usuario adcionar um novo conteudo no arquivo.
+
+        Console.WriteLine("Adicione um novo conteudo ao arquivo");
+        string novoTexto = Console.ReadLine();
+
+        File.WriteAllText(caminho, novoTexto);
+
+        Console.WriteLine("Alteração feita com sucesso!");
+
+    }
+}
+
